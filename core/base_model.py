dde62065d9cc4a1164ad9cf1a326387906a9cfe1
amjhd94 <110791799+amjhd94@users.noreply.github.com>
2022-08-17 09:35:52 -0400

    Update base_model.py

--- core/base_model.py
+++ core/base_model.py
@@ -1,13 +1,28 @@
 from tensorflow import keras
 
 class MyModel(keras.Model):
-    # {'activation':'swish', 'kernel_initializer':None} #activation = "swish", "elu", "softplus", "tanh"; All work fine in this case.
     def model(self, activation='swish', ker_initializer=None):
+        """
+
+        Parameters
+        ----------
+        activation : (optional: The default is 'swish') Activation function.
+            
+        ker_initializer : (optional: The default is None) Determines the selection 
+                            process of the initial weights of each layer.
+
+        Returns
+        -------
+        The individual NN model of the NN ensemble
+
+        """
         inputs = keras.Input(shape=(1,))
         x = keras.layers.Dense(8, activation=activation, kernel_initializer=ker_initializer)(inputs)
         x = keras.layers.Dense(32, activation=activation, kernel_initializer=ker_initializer)(x)
         x = keras.layers.Dense(16, activation=activation, kernel_initializer=ker_initializer)(x)
         x = keras.layers.Dense(8, activation=activation, kernel_initializer=ker_initializer)(x)
         outputs = keras.layers.Dense(1)(x)
+        
         self.model = keras.Model(inputs=inputs, outputs=outputs)
+        
         return self.model      
